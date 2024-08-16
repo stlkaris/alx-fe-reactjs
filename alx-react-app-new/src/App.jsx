a15570350722bc8109b58adf1154619139de8f02
Stella Kariuki <stlkariuki4@gmail.com>
2024-08-17 00:22:42 +0300

    new changes

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -7,6 +7,7 @@ import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
 import UserProfile from './components/UserProfile'
+import Counter from './components/Counter'
 
 
 function App() {
@@ -38,6 +39,7 @@ function App() {
       <WelcomeMessage />
       <Header />
       <MainContent />
+      <Counter />
       <Footer />
     </>
     
