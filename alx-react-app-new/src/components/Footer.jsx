9afa75a5a1c8d89c8999084150b0b5182a35423e
Stella Kariuki <stlkariuki4@gmail.com>
2024-08-17 00:06:32 +0300

    added changes

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+ function Footer () {
+    return (
+        <footer>
+        <p style = {{backgroundColor: 'red', textAlign: 'center'}}>© 2023 City Lovers</p>
+      </footer>
+    )
+ } 
+
+export default Footer;
