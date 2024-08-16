557a7710d26e93e36bd62875cc5615a5aeb68602
Stella Kariuki <stlkariuki4@gmail.com>
2024-08-16 23:31:47 +0300

    added files

---
+++ alx-react-app-new/alx-react-app/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+ function Footer () {
+    return (
+        <footer style={{ border: '1px solid gray', padding: '10px', margin: '10px' }}>
+        <p style= {{backgroundColor: 'blue', fontSize: '30px'}}>© 2023 City Lovers</p>
+      </footer>
+    )
+ } 
+
+export default Footer;
---
+++ alx-react-app-new/alx-react-app/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+ function MainContent () {
+    return (
+        <main style={{ border: '1px solid gray', padding: '10px', margin: '10px' }}>
+        <p style={{fontWeight: 'bold', backgroundColor: 'red'}}>I love to visit New York, Paris, and Tokyo.</p>
+      </main>
+    )
+ }
+
+export default MainContent;
