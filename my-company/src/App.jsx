4e663d07cad353a90aa3694a5c89c1313ce71d20
Stella Kariuki <stlkariuki4@gmail.com>
2024-08-18 21:11:30 +0300

    added files

---
+++ my-company/src/App.jsx
@@ -0,0 +1,30 @@
+
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './components/Home'
+import About from './components/About'
+import Services from './components/Services'
+import Contact from './components/Contact'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+
+
+const App = () => {
+  return (
+ <Router>
+    <div>
+      <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      <Footer />
+    </div>
+</Router>
+  );
+
+}
+
+export default App
