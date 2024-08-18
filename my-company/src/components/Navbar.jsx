706f8617c0b2506cd1bef9f45857ab2e367b8771
Stella Kariuki <stlkariuki4@gmail.com>
2024-08-18 21:45:18 +0300

    src/components/Navbar.jsx

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-   <nav style={{padding: '10px', backgroundColor: '#090979'}}>
+   <nav style={{padding: '10px', backgroundColor: '#090979', display: 'flex', justifyContent: 'center'}}>
     <Link to="/" style={{margin: '10px'}} >Home</Link>
     <Link to="/about" style={{margin: '10px'}}>About</Link>
     <Link to="/services" style={{margin: '10px'}}>Services</Link>
