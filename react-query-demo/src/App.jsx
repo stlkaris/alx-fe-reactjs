0296e77011d4028d77296d2a961f05ada001649e
Stella Kariuki <stlkariuki4@gmail.com>
2024-09-01 04:08:03 +0300

    added files

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import PostsComponent from './components/PostsComponent'
-// import {ReactQueryDevtools} from '@tanstack/react-query-devtools';
+ import {ReactQueryDevtools} from 'react-query/devtools';
 
   const queryClient = new QueryClient();
 
@@ -9,7 +9,7 @@ import PostsComponent from './components/PostsComponent'
     return (
       <QueryClientProvider client={queryClient}>
         <PostsComponent />
-        {/* <ReactQueryDevtools initialIsOpen={false}/> */}
+        { <ReactQueryDevtools initialIsOpen={false}/> }
       </QueryClientProvider>
     );
   }
