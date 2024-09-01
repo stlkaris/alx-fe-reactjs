4e7d67c56919f3efa4bd02132b1c2a2d79bebb22
Stella Kariuki <stlkariuki4@gmail.com>
2024-09-01 23:30:09 +0300

    added new folder and files

---
+++ react-todo/src/_tests_/TodoList.test.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import { render, fireEvent, getByText } from '@testing-library/react'
+import TodoList from '../components/TodoList'
+
+
+test('renders initial todos', () => {
+    const { getByText } = render(<TodoList />);
+    expect(getByText('Learn React')).toBeInTheDocument();
+    expect(getByText('Build a Todo List')).toBeInTheDocument();
+    expect(getByText('Write Tests')).toBeInTheDocument();
+});
+
+test('adds a new todo', () => {
+    const { getByPlaceholderText, getByText} = render(<TodoList />);
+    const input = getByPlaceholderText('Add a new todo')
+    const addButton = getByText('Add Todo');
+
+    fireEvent.change(input, {target: { value: 'New Todo'}})
+    fireEvent.click(addButton);
+    
+    expect(getByText('New Todo')).getByPlaceholderText
+});
+
+test('toggles todo completion', () => {
+    const {getByText} = render(<TodoList />);
+    const todo = getByText('Learn React')
+
+    fireEvent.click(todo);
+
+    expect(todo).toHaveStyle('text-decoration: line-through')
+
+    fireEvent.click(todo);
+
+    expect(todo).toHaveStyle('text-decoration: none')
+});
+
+test('deletes a todo', () => {
+    const {getByText, queryByText} = render(<TodoList />);
+    const todo = getByText('Learn React');
+    const deleteButton = todo.nextSibling;
+
+    fireEvent.click(deleteButton);
+    expect(queryByText('earn React')).not.toBeInTheDocument();
+});
+
+
---
+++ react-todo/src/components/TodoList.jsx
@@ -0,0 +1,58 @@
+import  React, { useState } from 'react'
+import AddTodoForm from './AddTodoForm';
+
+function TodoList () {
+    const [todos, setTodos] = useState( [
+        { id: 1, text: 'Learn React', completed: false},
+        { id: 2, text: 'Build a Todo List', completed: false},
+        { id: 3, text: 'Write Tests', completed: false}
+    ]);
+    
+    const addTodo = (text) => {
+        const newTodo = {
+            id: todos.length + 1,
+            text,
+            completed: false
+        }
+        setTodos([...todos, newTodos])
+    };
+
+    const toggleTodo = (id) => {
+        setTodos(
+            todos.map((todo) => 
+            todo.id === id ? {...todo, completed: !todo.completed}: todo
+            )
+        )
+    };
+
+    const deleteTodo = (id) => {
+        setTodos(todos.filter((todo) => todo.id !== id))
+
+    }
+
+
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <AddTodoForm addTodo={addTodo} />
+      <ul>
+        {todos.map((todo) => (
+            <li key={todo.id}>
+                <span
+                onClick={() => toggleTodo(todo.id)}
+                style={{
+                    textDecoration: todo.completed ? 'line-through' :'none',
+                    cursor: 'pointer',
+                }}>
+                  {todo.text}
+                </span>
+                <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+
+            </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default TodoList
