8c2363c310eb23a578151dceb80322cdd97bab43
Stella Kariuki <stlkariuki4@gmail.com>
2024-09-12 01:11:27 +0300

    styling changes

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -15,7 +15,7 @@ useEffect(() =>{
 return (
     <div className='container mx-auto p-4'>
         <h1 className='text-3xl font-bold mb-6 text-center'>FingerLicking Recipes</h1>
-        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
+        <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
             {recipes.map((recipe) => (
                 <div key={recipe.id} className='bg-white rounded-lg shadow-lg overflow-hidden hover:shadow-xl transition-shadow'>
                     <img src={recipe.image} alt={recipe.title} className='w-full h-48 object-cover' />
