d55164d89b1bcd356336616f0853295d47443ed7
Stella Kariuki <stlkariuki4@gmail.com>
2024-09-23 12:34:02 +0300

    added changes

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -2,6 +2,16 @@ import axios from 'axios';
 
 const GITHUB_SEARCH_API_URL = 'https://api.github.com/search/users?q=';
 
+  export const fetchUserData = async (username) => {
+    try {
+      const response = await axios.get(`${GITHUB_API_URL}${username}`);
+      return response.data;
+    } catch (error) {
+      console.error('Error fetching GitHub user:', error);
+      throw error; // We will handle this error in the component
+    }
+  };
+
 export const fetchAdvancedUserData = async (username) => {
   let query = username? `${username} in:login`: '';
 
